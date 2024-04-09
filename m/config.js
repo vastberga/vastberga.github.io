32f6a58bfdbf09ace0dcfa23fa8f645b74d97a4e
nomad <138936804+nomad-lol@users.noreply.github.com>
2024-04-09 13:38:38 +0200

    Update config.js

--- m/config.js
+++ m/config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/a/',
-    bare: 'https://gointerstellar.app/o/',
+    bare: 'https://phantomnetwork.cloud/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/m/handler.js',
